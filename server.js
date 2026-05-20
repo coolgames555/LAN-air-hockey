63a7c8b8a7af08780d9bdf808630e9f100cf4e7a
Moo Games <ivstiner@outlook.com>
2026-05-20 10:01:05 -0400

    Change puck velocity to fixed values

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ function resetPuck() {
     gameState.puck = {
         x: GAME_WIDTH / 2,
         y: GAME_HEIGHT / 2,
-        vx: Math.random() < 5 ? 3 : -3,
-        vy: Math.random() < 5 ? 2 : -2
+        vx: 15
+        vy: 15
     };
 }
 
